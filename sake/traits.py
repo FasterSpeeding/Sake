85b3d245651a49ba7925bcd1746f2e50771d1457
Faster Speeding <luke@lmbyrne.dev>
2020-10-14 14:46:14 +0100

    Add a redis implementation for the voice state resource

--- sake/traits.py
+++ sake/traits.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 __all__: typing.Final[typing.Sequence[str]] = [
+    "Cache",
     "Resource",
     "EmojiCache",
     "GuildCache",
@@ -395,3 +396,19 @@ class VoiceStateCache(Resource, typing.Protocol):
 
     async def set_voice_state(self, voice_state: voices.VoiceState) -> None:
         raise NotImplementedError
+
+
+class Cache(
+    GuildCache,
+    EmojiCache,
+    GuildChannelCache,
+    InviteCache,
+    MeCache,
+    MemberCache,
+    MessageCache,
+    PresenceCache,
+    RoleCache,
+    VoiceStateCache,
+    typing.Protocol,
+):
+    """Protocol of a cache which implements all the defined resources."""

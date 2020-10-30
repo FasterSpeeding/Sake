53ad781948377aa73823bb157f8782d8be3b464e
Faster Speeding <luke@lmbyrne.dev>
2020-10-30 18:47:24 +0000

    Remove the pickle marshal impl

--- sake/marshalling.py
+++ sake/marshalling.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 __all__: typing.Final[typing.Sequence[str]] = [
     "JSONMarshaller",
     "ObjectMarshaller",
-    "PickleMarshaller",
 ]
 
 import abc
@@ -11,7 +10,6 @@ import base64
 import datetime
 import json
 import logging
-import pickle
 import typing
 
 from hikari import applications
@@ -133,190 +131,6 @@ class ObjectMarshaller(abc.ABC, typing.Generic[ValueT]):
     @abc.abstractmethod
     def serialize_voice_state(self, voice_state: voices.VoiceState) -> ValueT:
         raise NotImplementedError
-
-
-class PickleMarshaller(ObjectMarshaller[bytes]):
-    __slots__: typing.Sequence[str] = ("_app",)
-
-    def __init__(self, app: traits.RESTAware) -> None:
-        self._app = app
-
-    @property
-    def protocol(self) -> int:
-        return 4
-
-    def _dumps(self, obj: typing.Any, *, fix_imports: bool = True, buffer_callback: typing.Any = None) -> bytes:
-        return pickle.dumps(obj, protocol=self.protocol, fix_imports=fix_imports, buffer_callback=buffer_callback)
-
-    def _loads(
-        self,
-        data: bytes,
-        /,
-        *,
-        fix_imports: bool = True,
-        encoding: str = "ASCII",
-        errors: str = "strict",
-        buffers: typing.Any = None,
-    ) -> typing.Any:
-        return pickle.loads(data, fix_imports=fix_imports, encoding=encoding, errors=errors, buffers=buffers)
-
-    def deserialize_emoji(self, value: bytes) -> emojis.KnownCustomEmoji:
-        emoji = self._loads(value)
-
-        if not isinstance(emoji, emojis.KnownCustomEmoji):
-            raise ValueError(f"Unexpected object type {type(emoji)}, expected a KnownCustomEmoji")
-
-        emoji.app = self._app
-
-        if emoji.user is not None:
-            emoji.user.app = self._app
-
-        return emoji
-
-    def serialize_emoji(self, emoji: emojis.KnownCustomEmoji) -> bytes:
-        return self._dumps(emoji)
-
-    def deserialize_guild(self, value: bytes) -> guilds.GatewayGuild:
-        guild = self._loads(value)
-
-        if not isinstance(guild, guilds.GatewayGuild):
-            raise ValueError(f"Unexpected object type {type(guild)}, expected a GatewayGuild")
-
-        guild.app = self._app
-        return guild
-
-    def serialize_guild(self, guild: guilds.GatewayGuild) -> bytes:
-        return self._dumps(guild)
-
-    def deserialize_guild_channel(self, value: bytes) -> channels.GuildChannel:
-        channel = self._loads(value)
-
-        if not isinstance(channel, channels.GuildChannel):
-            raise ValueError(f"Unexpected object type {type(channel)}, expected a GuildChannel")
-
-        channel.app = self._app
-        return channel
-
-    def serialize_guild_channel(self, channel: channels.GuildChannel) -> bytes:
-        return self._dumps(channel)
-
-    def deserialize_invite(self, value: bytes) -> invites.InviteWithMetadata:
-        invite = self._loads(value)
-
-        if not isinstance(invite, invites.InviteWithMetadata):
-            raise ValueError(f"Unexpected object type {type(invite)}, expected a InviteWithMetadata")
-
-        invite.app = self._app
-
-        if invite.target_user is not None:
-            invite.target_user.app = self._app
-
-        if invite.inviter is not None:
-            invite.inviter.app = self._app
-
-        return invite
-
-    def serialize_invite(self, invite: invites.InviteWithMetadata) -> bytes:
-        return self._dumps(invite)
-
-    def deserialize_me(self, value: bytes) -> users.OwnUser:
-        me = self._loads(value)
-
-        if not isinstance(me, users.OwnUser):
-            raise ValueError(f"Unexpected object type {type(me)}, expected a OwnUser")
-
-        me.app = self._app
-        return me
-
-    def serialize_me(self, me: users.OwnUser) -> bytes:
-        return self._dumps(me)
-
-    def deserialize_member(self, value: bytes) -> guilds.Member:
-        member = self._loads(value)
-
-        if not isinstance(member, guilds.Member):
-            raise ValueError(f"Unexpected object type {type(member)}, expected Member")
-
-        member.user.app = self._app
-        return member
-
-    def serialize_member(self, member: guilds.Member) -> bytes:
-        return self._dumps(member)
-
-    def deserialize_message(self, value: bytes) -> messages.Message:
-        message = self._loads(value)
-
-        if not isinstance(message, messages.Message):
-            raise ValueError(f"Unexpected object type {type(message)}, expected Message")
-
-        if message.member is not None:
-            message.member.user.app = self._app
-
-        if message.application is not None:
-            message.application.app = self._app
-
-        if message.message_reference is not None:
-            message.message_reference.app = self._app
-
-        message.author.app = self._app
-        return message
-
-    def serialize_message(self, message: messages.Message) -> bytes:
-        return self._dumps(message)
-
-    def deserialize_presence(self, value: bytes) -> presences.MemberPresence:
-        presence = self._loads(value)
-
-        if not isinstance(presence, presences.MemberPresence):
-            raise ValueError(f"Unexpected object type {type(presence)}, expected a MemberPresence")
-
-        presence.app = self._app
-
-        for activity in presence.activities:
-            if isinstance(activity.emoji, emojis.KnownCustomEmoji):
-                activity.emoji.app = self._app
-
-        return presence
-
-    def serialize_presence(self, presence: presences.MemberPresence) -> bytes:
-        return self._dumps(presence)
-
-    def deserialize_role(self, value: bytes) -> guilds.Role:
-        role = self._loads(value)
-
-        if not isinstance(role, guilds.Role):
-            raise ValueError(f"Unexpected object type {type(role)}, expected a Role")
-
-        role.app = self._app
-        return role
-
-    def serialize_role(self, role: guilds.Role) -> bytes:
-        return self._dumps(role)
-
-    def deserialize_user(self, value: bytes) -> users.User:
-        user = self._loads(value)
-
-        if not isinstance(user, users.User):
-            raise ValueError(f"Unexpected object type {type(user)}, expected a User impl")
-
-        user.app = self._app
-        return user
-
-    def serialize_user(self, user: users.User) -> bytes:
-        return self._dumps(user)
-
-    def deserialize_voice_state(self, value: bytes) -> voices.VoiceState:
-        voice_state = self._loads(value)
-
-        if not isinstance(voice_state, voices.VoiceState):
-            raise ValueError(f"Unexpected object type {type(voice_state)}, expected a VoiceState")
-
-        voice_state.app = self._app
-        voice_state.member.user.app = self._app
-        return voice_state
-
-    def serialize_voice_state(self, voice_state: voices.VoiceState) -> bytes:
-        return self._dumps(voice_state)
 
 
 def _get_init_name(field: str) -> str:

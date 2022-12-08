15477a87d13250712ed2925a35ff646cd77712a8
Lucina <lucina@lmbyrne.dev>
2022-12-08 19:04:38 +0000

    Some small CI improvements (#175)

--- noxfile.py
+++ noxfile.py
@@ -48,11 +48,7 @@ nox.options.sessions = [
     "test",
     "verify-types",
 ]
-GENERAL_TARGETS = ["./noxfile.py", "./tests"]
-TOP_LEVEL_TARGETS = ["./sake", "./tests", "./noxfile.py"]
-for path in pathlib.Path("./sake").glob("**/*.py"):
-    GENERAL_TARGETS.append(str(path))
-
+TOP_LEVEL_TARGETS = ["./examples", "./noxfile.py", "./sake", "./tests"]
 
 _DEV_DEP_DIR = pathlib.Path("./dev-requirements")
 
@@ -185,7 +181,7 @@ def flake8(session: nox.Session) -> None:
     """Run this project's modules against the pre-defined flake8 linters."""
     install_requirements(session, *_dev_dep("flake8"))
     session.log("Running flake8")
-    session.run("pflake8", *GENERAL_TARGETS, log=False)
+    session.run("pflake8", *TOP_LEVEL_TARGETS, log=False)
 
 
 @nox.session(reuse_venv=True, name="slot-check")
